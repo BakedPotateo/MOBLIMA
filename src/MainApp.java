d69b9b8e2aac62064551399f37c013ca0f3864ce
BakedPotateo <ryanteo2991@gmail.com>
2022-10-30 05:10:53 +0800

    Created classes

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,42 @@
+import java.util.*;
+
 public class MainApp {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to MOBLIMA!");
+        Scanner sc = new Scanner(System.in);
+        int choice = 0;
+
+        while(choice != 3){
+            System.out.println("Welcome to MOBLIMA!\n");
+
+            System.out.println("Please enter your choice:");
+            System.out.println("1. Customer\n2. Staff\n3. Exit\n");
+
+            /*
+             * Check if input is an integer
+             */
+            while (!sc.hasNextInt()) {
+            	System.out.println("Invalid input type. Please enter an integer value.");
+        		sc.next(); // remove newline
+            }
+            
+            choice = sc.nextInt();
+            switch(choice){
+                case 1:
+                    // Customer app
+                    System.out.println("CUstomer app");
+                    break;
+                case 2:
+                    // Staff app
+                    System.out.println("Staff app");
+                    break;
+                case 3:
+                    System.out.println("Thank you for using MOBLIMA!");
+                    break;
+                default:
+                    System.out.println("Please enter an integer between 1-3.");
+                    break;
+            }
+        }
+        sc.close();
     }
 }
