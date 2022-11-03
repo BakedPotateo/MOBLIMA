2b63e130af227993fc77b964b7cd9cd685c337cc
Aloyloyloyloy <aloysiustayz@yahoo.com.sg>
2022-11-03 20:14:26 +0800

    need the user and cinema class

--- src/Tickets/Ticket.java
+++ src/Tickets/Ticket.java
@@ -1,10 +1,42 @@
 package Tickets;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Ticket {
     private String typeOfMovie;
-    private String ageOfMovieGoer;
     private double price;
-    // TODO CinemaType
+    // private User user; //get from userclass
+    // private Cinema cinemaType; //get from cinemaclass
     private LocalDate dayOfWeek;
+    private LocalTime timeOfMovie;
+
+
+    public String getTypeOfMovie() {
+        return typeOfMovie;
+    }
+    public void setTypeOfMovie(String typeOfMovie) {
+        this.typeOfMovie = typeOfMovie;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public String getDayOfWeek() {
+        return dayOfWeek.format(DateTimeFormatter.ofPattern("EEEE, dd/MM/yyyy"));
+    }
+    public void setDayOfWeek(LocalDate dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+    public String getTimeOfMovie() {
+        return timeOfMovie.format(DateTimeFormatter.ofPattern("HH:mm"));
+    }
+    public void setTimeOfMovie(LocalTime timeOfMovie) {
+        this.timeOfMovie = timeOfMovie;
+    }
+
+    
 }
