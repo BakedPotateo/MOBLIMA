77300cba60f9a16017d09c874b3ad62f7c2ce189
BakedPotateo <ryanteo2991@gmail.com>
2022-11-06 21:33:01 +0800

    Commit suicide

--- src/Managers/SettingsManager.java
+++ src/Managers/SettingsManager.java
@@ -1,13 +1,19 @@
 package Managers;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class SettingsManager{
+import utils.ProjectRootPathFinder;
+
+public class SettingsManager {
     // Attributes
 
-    public Scanner sc = new Scanner(System.in);
+    private Scanner sc = new Scanner(System.in);
     /*
-     * instance checks whether Settings has been instantiated before. Static variable is the same between objects of the same class.
+     * instance checks whether MovieManager has been instantiated before. Static variable is the same between objects of the same class.
      */
     public static SettingsManager instance = null;
 
@@ -16,10 +22,13 @@ public class SettingsManager{
      */
     private SettingsManager(){}
 
+    public final static String TICKETS_FILE = ProjectRootPathFinder.findProjectRootPath() + "/Database/Tickets/tickets.txt";
+    public final static String HOLIDAY_FILE = ProjectRootPathFinder.findProjectRootPath() + "/Database/Settings/holidays.txt";
+
     // Public methods
 
     /*
-     * getInstance checks if Settings has been instantiated before. 
+     * getInstance checks if SettingsManager has been instantiated before. 
      * If no previous instance was created, it creates a new one, 
      * else it will use the original instance.
      */
@@ -30,80 +39,14 @@ public class SettingsManager{
         return instance;
     }
 
-    public void displaySettingsMenu(){
-        int choice = 0;
-        while(choice != 3){
-            System.out.println("--------- SETTINGS MENU ---------\n"
-                              +" 1. Configure ticket prices\n"
-                              +" 2. Configure holidays\n"
-                              +" 3. Exit\n"
-                              +"---------------------------------\n");
-
-            System.out.println("Please enter your choice:");
-
-            /*
-             * Check if input is an integer
-             */
-            while (!sc.hasNextInt()) {
-            	System.out.println("Invalid input type. Please enter an integer value.");
-        		sc.next(); // remove newline
-            }
-
-            choice = sc.nextInt();
-            switch(choice){
-                case 1:
-                    this.configTicketPrices();
-                    break;
-                case 2:
-                    this.configHolidays();
-                    break;
-                case 3:
-                    System.out.println("Exiting...");
-                default:
-                    System.out.println("Please enter an integer between 1-3");
-            }
-        }
+    @SuppressWarnings("unchecked")
+    public ArrayList<String> read() {
+        try {
+            ObjectInputStream objectinputstream = new ObjectInputStream(new FileInputStream(TICKETS_FILE));
+            ArrayList<String> movieList = (ArrayList<String>) objectinputstream.readObject();
+            objectinputstream.close();
+            return movieList;
+        } catch (ClassNotFoundException | IOException e) {}
+        return new ArrayList<String>();
     }
-
-    private void configTicketPrices(){}
-
-    private void configHolidays(){
-        int choice = 0;
-        while(choice != 3){
-            System.out.println("--------- HOLIDAYS MENU ---------\n"
-                              +" 1. Add holiday\n"
-                              +" 2. Delete holiday\n"
-                              +" 3. Show all holidays\n"
-                              +" 4. Exit\n"
-                              +"---------------------------------\n");
-
-            System.out.println("Please enter your choice:");
-
-            /*
-             * Check if input is an integer
-             */
-            while (!sc.hasNextInt()) {
-            	System.out.println("Invalid input type. Please enter an integer value.");
-        		sc.next(); // remove newline
-            }
-
-            choice = sc.nextInt();
-            switch(choice){
-                case 1:
-                    // add holiday
-                    break;
-                case 2:
-                    // delete holiday
-                    break;
-                case 3:
-                    // show holidays
-                    break;
-                default:
-                    System.out.println("Please enter an integer between 1-3");
-            }
-        }
-    }
-
-
-    
 }
---
+++ src/Staff/SettingsApp.java
@@ -0,0 +1,109 @@
+package Staff;
+
+import java.util.Scanner;
+
+public class SettingsApp{
+    // Attributes
+
+    public Scanner sc = new Scanner(System.in);
+    /*
+     * instance checks whether Settings has been instantiated before. Static variable is the same between objects of the same class.
+     */
+    public static SettingsApp instance = null;
+
+    /*
+     * Empty class constructor
+     */
+    private SettingsApp(){}
+
+    // Public methods
+
+    /*
+     * getInstance checks if Settings has been instantiated before. 
+     * If no previous instance was created, it creates a new one, 
+     * else it will use the original instance.
+     */
+    public static SettingsApp getInstance()
+    {
+        if (instance == null)
+            instance = new SettingsApp(); // instance is a static variable
+        return instance;
+    }
+
+    public void displaySettingsMenu(){
+        int choice = 0;
+        while(choice != 3){
+            System.out.println("--------- SETTINGS MENU ---------\n"
+                              +" 1. Configure ticket prices\n"
+                              +" 2. Configure holidays\n"
+                              +" 3. Exit\n"
+                              +"---------------------------------\n");
+
+            System.out.println("Please enter your choice:");
+
+            /*
+             * Check if input is an integer
+             */
+            while (!sc.hasNextInt()) {
+            	System.out.println("Invalid input type. Please enter an integer value.");
+        		sc.next(); // remove newline
+            }
+
+            choice = sc.nextInt();
+            switch(choice){
+                case 1:
+                    this.configTicketPrices();
+                    break;
+                case 2:
+                    this.configHolidays();
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                default:
+                    System.out.println("Please enter an integer between 1-3");
+            }
+        }
+    }
+
+    private void configTicketPrices(){}
+
+    private void configHolidays(){
+        int choice = 0;
+        while(choice != 3){
+            System.out.println("--------- HOLIDAYS MENU ---------\n"
+                              +" 1. Add holiday\n"
+                              +" 2. Delete holiday\n"
+                              +" 3. Show all holidays\n"
+                              +" 4. Exit\n"
+                              +"---------------------------------\n");
+
+            System.out.println("Please enter your choice:");
+
+            /*
+             * Check if input is an integer
+             */
+            while (!sc.hasNextInt()) {
+            	System.out.println("Invalid input type. Please enter an integer value.");
+        		sc.next(); // remove newline
+            }
+
+            choice = sc.nextInt();
+            switch(choice){
+                case 1:
+                    // add holiday
+                    break;
+                case 2:
+                    // delete holiday
+                    break;
+                case 3:
+                    // show holidays
+                    break;
+                default:
+                    System.out.println("Please enter an integer between 1-3");
+            }
+        }
+    }
+
+
+    
+}
