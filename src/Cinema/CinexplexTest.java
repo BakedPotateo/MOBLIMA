16d2974dd5d5fb75f431d9bfac51799f4f82258f
Aloyloyloyloy <aloysiustayz@yahoo.com.sg>
2022-11-06 16:12:17 +0800

    added some stuff to CinemaManager

--- src/Cinema/CinexplexTest.java
+++ src/Cinema/CinexplexTest.java
@@ -8,17 +8,22 @@ public class CinexplexTest {
         CinemaManager manager = CinemaManager.getInstance();
         ArrayList<Cinema> cinemas = new ArrayList<Cinema>(); 
         SeatingLayout layout = new SeatingLayout(5, 10);
-        Cinema cinema1 = new Cinema("H1", "Normal", layout);
-        Cinema cinema2 = new Cinema("H2", "Normal", layout);
-        Cinema cinema3 = new Cinema("H3", "IMAX", layout);
-        cinemas.add(cinema1);
-        cinemas.add(cinema2);
-        cinemas.add(cinema3);
-        manager.createCineplex("Shaws Theatre", cinemas);
+        // Cinema cinema1 = new Cinema("H1", "Normal", layout);
+        // Cinema cinema2 = new Cinema("H2", "Normal", layout);
+        // Cinema cinema3 = new Cinema("H3", "IMAX", layout);
+        // cinemas.add(cinema1);
+        // cinemas.add(cinema2);
+        // cinemas.add(cinema3);
+        // manager.createCineplex("Shaws Theatre", cinemas);
+        // manager.createCineplex("Golden Village", cinemas);
         // manager.createCinema("Golden Village", "G1", "Gold Class", layout);
-        manager.createCinema("Shaw Theatres", "S1", "IMAX", layout);
+        // manager.createCinema("Shaws Theatre", "S1", "IMAX", layout);
         ArrayList<Cineplex> cineplexes = manager.read();
-
+        for (Cineplex cineplex : cineplexes) {
+            System.out.println(cineplex.makeString());
+        }
+        manager.deleteCinema("G1");
+        cineplexes = manager.read();
         for (Cineplex cineplex : cineplexes) {
             System.out.println(cineplex.makeString());
         }
