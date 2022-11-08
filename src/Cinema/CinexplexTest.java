ff519324c398ca74b76a33fa976d560872ed4ae5
Aloyloyloyloy <aloysiustayz@yahoo.com.sg>
2022-11-08 23:13:22 +0800

    commit suicide

--- src/Cinema/CinexplexTest.java
+++ src/Cinema/CinexplexTest.java
@@ -7,25 +7,31 @@ public class CinexplexTest {
     public static void main(String[] args) {
         CinemaManager manager = CinemaManager.getInstance();
         ArrayList<Cinema> cinemas = new ArrayList<Cinema>(); 
-        SeatingLayout layout = new SeatingLayout(5, 10);
-        // Cinema cinema1 = new Cinema("H1", "Normal", layout);
-        // Cinema cinema2 = new Cinema("H2", "Normal", layout);
-        // Cinema cinema3 = new Cinema("H3", "IMAX", layout);
-        // cinemas.add(cinema1);
-        // cinemas.add(cinema2);
-        // cinemas.add(cinema3);
-        // manager.createCineplex("Shaws Theatre", cinemas);
-        // manager.createCineplex("Golden Village", cinemas);
-        // manager.createCinema("Golden Village", "G1", "Gold Class", layout);
-        // manager.createCinema("Shaws Theatre", "S1", "IMAX", layout);
+        SeatingLayout layout1 = new SeatingLayout(5, 10);
+        SeatingLayout layout2 = new SeatingLayout(3, 15);
+        SeatingLayout layout3 = new SeatingLayout(7, 8);
+        Cinema cinema1 = new Cinema("H1", "Normal", layout1);
+        Cinema cinema2 = new Cinema("H2", "Normal", layout2);
+        Cinema cinema3 = new Cinema("H3", "IMAX", layout3);
+        cinemas.add(cinema1);
+        cinemas.add(cinema2);
+        cinemas.add(cinema3);
+        manager.createCineplex("Shaws Theatre", cinemas);
+        manager.createCineplex("Golden Village", cinemas);
+        manager.createCinema("Golden Village", "G1", "Gold Class", layout3);
+        manager.createCinema("Shaws Theatre", "S1", "IMAX", layout3);
         ArrayList<Cineplex> cineplexes = manager.read();
         for (Cineplex cineplex : cineplexes) {
             System.out.println(cineplex.makeString());
         }
-        manager.deleteCinema("G1");
-        cineplexes = manager.read();
-        for (Cineplex cineplex : cineplexes) {
-            System.out.println(cineplex.makeString());
-        }
+
+        Cinema gvG1 = manager.searchCinema("G1");
+        // gvG1.getLayout().printLayout();
+        SeatingLayout gvG1Layout = gvG1.getLayout();
+        gvG1Layout.assignSeat(28);
+        gvG1Layout.assignSeat(32);
+        gvG1Layout.assignSeat(49);
+        gvG1Layout.unassignSeat(49);
+        gvG1Layout.printLayout();
     }
 }
